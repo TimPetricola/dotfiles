52ce188ed6eb2ae3a5bcc3494fd5fa9258ba2b1d
Tim Petricola <tim.petricola@gmail.com>
2015-10-19 16:49:43 +0200

    Remove duplication of h and c

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,1 @@
 alias reload!='. ~/.zshrc'
-
-# Use c to go to the Projects folder
-c() { cd ~/Projects/$1; }
-_c() { _files -W ~/Projects -/; }
-compdef _c c
-
-# And h to go to home folder
-h() { cd ~/$1; }
-_h() { _files -W ~/ -/; }
-compdef _h h
