ca0682ba3772df716a8a4e528571f72afc005570
Tim Petricola <tim.petricola@gmail.com>
2015-12-30 18:22:11 +0100

    Shorter prompt when working within Projects

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,8 +1,5 @@
 autoload colors && colors
 
-# Text to display if the branch is dirty
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}*%{$reset_color%}"
-
 git_dirty() {
   st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
   echo -n "%{$reset_color%}"
@@ -30,15 +27,26 @@ ruby_prompt_info() {
   echo -n "%{$fg[magenta]%}${RUBY_ROOT##*/}%{$reset_color%}"
 }
 
-path_prompt_info() {
-  echo -n " "
-  echo -n "%{$fg[blue]%}%~%{$reset_color%}"
-}
-
-user_prompt_info() {
-  echo "%n@%m"
+dir_prompty_info() {
+  if [[ "${PWD##$PROJECTS}" == "${PWD}" || "${PWD##$PROJECTS}" == "" ]]
+  then
+    # Not in a Projects directory
+    echo -n "%n@%m"
+    echo -n " "
+    echo -n "%{$fg[blue]%}%~%{$reset_color%}"
+  else
+    project=${${PWD##$PROJECTS}#/}
+    echo -n "[%{$fg[yellow]%}"
+    if [[ "${project}" == "" ]]
+    then
+      echo -n "/"
+    else
+      echo -n ${project}
+    fi
+    echo -n "%{$reset_color%}]"
+  fi
 }
 
 PROMPT='
-$(user_prompt_info)$(path_prompt_info)$(ruby_prompt_info)$(git_dirty)
+$(dir_prompty_info)$(ruby_prompt_info)$(git_dirty)
 $ '
