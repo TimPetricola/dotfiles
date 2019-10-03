2f4176190ec646a1a3f550ea5875622abb8becf3
Tim Petricola <tim.petricola@gmail.com>
2019-10-03 10:17:56 +0200

    Update brew casks list

--- homebrew-casks/install.sh
+++ homebrew-casks/install.sh
@@ -3,30 +3,33 @@
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-brew cask install visual-studio-code
-brew cask install iterm2
+brew cask install google-chrome
+brew cask install firefox
 brew cask install 1password
+brew cask install iterm2
+brew cask install spotify
+brew cask install visual-studio-code
 brew cask install alfred
 brew cask install slack
-brew cask install spotify
 brew cask install vlc
 brew cask install nordvpn
 brew cask install franz
 brew cask install spectacle
 brew cask install sketch
-brew cask install abstract
 brew cask install rescuetime
 brew cask install transmission
 brew cask install clipy
 brew cask install disk-inventory-x
 brew cask install calibre
-brew cask install sequel-pro
 brew cask install imageoptim
 brew cask install github
 brew cask install google-backup-and-sync
 brew cask install cloudapp
 brew cask install cheatsheet
 brew cask install mtmr
-
 brew cask install cleanmymac
-brew cask install flux
+brew cask install google-backup-and-sync
+brew cask install charles
+brew cask install insomnia
+brew cask install notion
+brew cask install telegram
