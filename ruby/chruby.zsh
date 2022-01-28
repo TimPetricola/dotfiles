d2eb9da3f6dffbdb73470a6855be66d8815ec1cb
Tim Petricola <tim.petricola@gmail.com>
2022-01-28 22:02:14 +0100

    chruby cleanup

--- ruby/chruby.zsh
+++ ruby/chruby.zsh
@@ -1,1 +1,7 @@
-source /usr/local/opt/chruby/share/chruby/chruby.sh
+if [[ -e /opt/homebrew/share/chruby ]]; then
+  source /opt/homebrew/share/chruby/chruby.sh
+  source /opt/homebrew/share/chruby/auto.sh
+  if [[ -f ~/.ruby-version ]]; then
+    chruby $(cat ~/.ruby-version)
+  fi
+fi
